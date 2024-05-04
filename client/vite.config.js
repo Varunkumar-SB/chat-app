d0c6b1d1ff81f954f5d02501efc6f0cecfe39ed7
Varunkumar S B <sbvarunkumar05@gmail.com>
2024-05-05 02:07:52 +0530

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://able-badly-chipmunk.ngrok-free.app:3000",
       },
     },
   },
