5f50fae35297a3f0be1ce6253724c5946c20aedd
Varunkumar S B <sbvarunkumar05@gmail.com>
2024-05-05 02:55:21 +0530

    url fix

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:3000", {
+      const socket = io("https://able-badly-chipmunk.ngrok-free.app", {
         query: {
           userId: authUser._id,
         },
