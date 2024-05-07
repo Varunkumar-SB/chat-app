fd32b2d4773181db353895f7e535d6ba51eb4d57
Varunkumar S B <sbvarunkumar05@gmail.com>
2024-05-07 09:18:50 +0530

    test

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 app.use(morgan("dev"));
 app.use(cookieParser());
 
